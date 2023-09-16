0bba941dcd41e5c4327cc32701c23fabe50f6368
Chris Clegg <cleggct@gmail.com>
2023-09-15 20:35:17 -0700

    got the favicon working

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,11 +22,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-
-      <head>
-        <link rel='icon' href='/favicon.ico'/>
-      </head>
-
       <body>{children}</body>
     </html>
   )
